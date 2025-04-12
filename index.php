77b6ee37b53ad1d56e4297b11103f708a488c47e
Vesna <vesnaerkic@gmail.com>
2025-04-12 18:19:23 +0200

    partials

--- index.php
+++ index.php
@@ -1,47 +1,14 @@
-<?php include "header.php"; ?>
+<?php include "partials/header.php"; ?>
 
-<!-- header -->
-<header class="homeHeader">
-    <div class="hroup">
-        <h1>Dobrodošli na EKOHEROJ portal</h1>
-        <div class="buttons text-center">
-            <a href="" class="btn btn-warning">O nama</a>
-            <a href="" class="btn btn-success">Kontakt</a>
-        </div>
-    </div>
-</header>
+<?php include "partials/home-header.php"; ?>
 
-<!-- o nama -->
- <div class="onama py-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3 text-center">
-                <img src="img/vector1.png" alt="">
-                <h4>Saradnja sa nama</h4>
-                <p>Pridružite nam se u očuvanju prirode – zajedno možemo više!</p>
-            </div>
-            <div class="col-md-3 text-center">
-                <img src="img/vector2.png" alt="">
-                <h4>Saradnja sa nama</h4>
-                <p>Pridružite nam se u očuvanju prirode – zajedno možemo više!</p>
-            </div>
-            <div class="col-md-3 text-center">
-                <img src="img/vector3.png" alt="">
-                <h4>Saradnja sa nama</h4>
-                <p>Pridružite nam se u očuvanju prirode – zajedno možemo više!</p>
-            </div>
-            <div class="col-md-3 text-center">
-                <img src="img/vector4.png" alt="">
-                <h4>Saradnja sa nama</h4>
-                <p>Pridružite nam se u očuvanju prirode – zajedno možemo više!</p>
-            </div>
-        </div>
-    </div>
- </div>
+<?php include "partials/onama-home.php"; ?>
+
+<?php include "partials/swiper-slider.php"; ?>
+
+
  
- 
- 
-<?php include "footer.php";?>
+<?php include "partials/footer.php";?>
 
 
   
