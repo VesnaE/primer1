07cab9735f1cd529ff7c384b9fce32a806d8a0d6
Vesna <vesnaerkic@gmail.com>
2025-04-12 17:49:33 +0200

    o nama i instalacija swiper slider

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include "header.php"; ?>
 
-
+<!-- header -->
 <header class="homeHeader">
     <div class="hroup">
         <h1>Dobrodošli na EKOHEROJ portal</h1>
@@ -10,6 +10,34 @@
         </div>
     </div>
 </header>
+
+<!-- o nama -->
+ <div class="onama py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 text-center">
+                <img src="img/vector1.png" alt="">
+                <h4>Saradnja sa nama</h4>
+                <p>Pridružite nam se u očuvanju prirode – zajedno možemo više!</p>
+            </div>
+            <div class="col-md-3 text-center">
+                <img src="img/vector2.png" alt="">
+                <h4>Saradnja sa nama</h4>
+                <p>Pridružite nam se u očuvanju prirode – zajedno možemo više!</p>
+            </div>
+            <div class="col-md-3 text-center">
+                <img src="img/vector3.png" alt="">
+                <h4>Saradnja sa nama</h4>
+                <p>Pridružite nam se u očuvanju prirode – zajedno možemo više!</p>
+            </div>
+            <div class="col-md-3 text-center">
+                <img src="img/vector4.png" alt="">
+                <h4>Saradnja sa nama</h4>
+                <p>Pridružite nam se u očuvanju prirode – zajedno možemo više!</p>
+            </div>
+        </div>
+    </div>
+ </div>
  
  
  
